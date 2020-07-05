bdb493f86481f0663a43c17162e5c06082d90250
Jaime Blasco <git@jaimeblasco.com>
2020-07-05 14:46:38 +0200

    Add nested scroll example

---
+++ example/lib/modals/modal_with_nested_scroll.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NestedScrollModal extends StatelessWidget {
+  final ScrollController scrollController;
+
+  const NestedScrollModal({Key key, this.scrollController}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return NestedScrollView(
+      
+        physics: ScrollPhysics(parent: PageScrollPhysics()),
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  Container(height: 300, color: Colors.blue),
+                ],
+              ),
+            ),
+          ];
+        },
+        body: ListView.builder(
+           controller: scrollController,
+          itemBuilder: (context, index) {
+            return Container(
+              height: 100,
+              color: index.isOdd ? Colors.green : Colors.orange,
+            );
+          },
+          itemCount: 12,
+        ));
+  }
+}
