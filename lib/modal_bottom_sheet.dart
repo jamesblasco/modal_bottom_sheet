da2cec14a483657c96036f0db23e10e014d93c87
Jaime Blasco <git@jaimeblasco.com>
2020-07-05 14:07:34 +0200

    Leave only the screenshots

--- lib/modal_bottom_sheet.dart
+++
@@ -1,6 +0,0 @@
-export 'src/bottom_sheet.dart';
-export 'src/bottom_sheet_route.dart';
-export 'src/material_with_modal_page_route.dart';
-export 'src/bottom_sheets/cupertino_bottom_sheet.dart';
-export 'src/bottom_sheets/material_bottom_sheet.dart';
-export 'src/bottom_sheets/bar_bottom_sheet.dart';
