b27b17f9e8440254232e40ea68f039502fa9ccdf
minnu <35523357+yahu1031@users.noreply.github.com>
2020-09-13 04:26:15 +0530

    rewritten using ternary operators 😜.

--- lib/src/bottom_sheet.dart
+++ lib/src/bottom_sheet.dart
@@ -301,16 +301,16 @@ class _ModalBottomSheetState extends State<ModalBottomSheet>
 // Otherwise the calculate the velocity with a VelocityTracker
       if (_velocityTracker == null) {
         // Checking the device type as per the OS installed in it
-        // SmartPhone Devices
-        if (Platform.isAndroid || Platform.isIOS) {
-          _velocityTracker = VelocityTracker(PointerDeviceKind.touch);
-        }
-        // PCs or desktops or Laptops devices has mouse pointers
-        else if (Platform.isLinux || Platform.isWindows || Platform.isMacOS) {
-          _velocityTracker = VelocityTracker(PointerDeviceKind.mouse);
-        } else {
-          _velocityTracker = VelocityTracker(PointerDeviceKind.unknown);
-        }
+        _velocityTracker = VelocityTracker(
+          // SmartPhone Devices
+          (Platform.isAndroid || Platform.isIOS)
+              ? PointerDeviceKind.touch
+              : // PCs or desktops or Laptops devices has mouse pointers
+              (Platform.isLinux || Platform.isWindows || Platform.isMacOS)
+                  ? VelocityTracker(PointerDeviceKind.mouse)
+                  : // Some unknown devices
+                  VelocityTracker(PointerDeviceKind.unknown),
+        );
         _startTime = DateTime.now();
       }
       DragUpdateDetails dragDetails;
