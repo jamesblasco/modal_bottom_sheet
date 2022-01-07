d350f4b66fcb11c3178fc69f09005de16f9ef7d7
MindMayhem <39557395+MindMayhem@users.noreply.github.com>
2022-01-08 01:02:59 +0300

    Hero animation support
    Simple solution to work Hero animation with modal_bottom_sheet

--- lib/src/bottom_sheet_route.dart
+++ lib/src/bottom_sheet_route.dart
@@ -127,7 +127,7 @@ class _ModalBottomSheetState<T> extends State<_ModalBottomSheet<T>> {
   }
 }
 
-class ModalBottomSheetRoute<T> extends PopupRoute<T> {
+class ModalBottomSheetRoute<T> extends PageRoute<T> {
   ModalBottomSheetRoute({
     this.closeProgressThreshold,
     this.containerBuilder,
@@ -168,7 +168,13 @@ class ModalBottomSheetRoute<T> extends PopupRoute<T> {
 
   @override
   bool get barrierDismissible => isDismissible;
+  
+  @override
+  bool get maintainState => false; //idk but needed
 
+  @override
+  bool get opaque => false; //transparency
+  
   @override
   final String? barrierLabel;
 
