b229583f9de3d9a65f5d444d72ae4a63836cb6e1
Jaime Blasco <jaimeblasco97@gmail.com>
2022-09-09 11:33:14 +0200

    feat: add foldable example

--- sheet/example/android/app/src/main/kotlin/jb/example/example/MainActivity.kt
+++ sheet/example/android/app/src/main/kotlin/jb/example/example/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.example
+package jb.example.example
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ sheet/example/lib/examples/sheet/fold_screen_sheet.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:sheet/sheet.dart';
+
+class FoldableScreenFloatingSheet extends StatefulWidget {
+  @override
+  _FitSheetState createState() => _FitSheetState();
+}
+
+class _FitSheetState extends State<FoldableScreenFloatingSheet> {
+  final SheetController controller = SheetController();
+
+  @override
+  void initState() {
+    Future<void>.delayed(const Duration(milliseconds: 400), animateSheet);
+
+    super.initState();
+  }
+
+  void animateSheet() {
+    controller.relativeAnimateTo(1,
+        duration: const Duration(milliseconds: 400), curve: Curves.easeOut);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DisplayFeatureSubScreen(
+      anchorPoint: Offset.infinite,
+      child: Sheet.raw(
+        physics: const SnapSheetPhysics(
+          parent: BouncingSheetPhysics(overflowViewport: false),
+          stops: <double>[0, 1],
+        ),
+        padding: const EdgeInsets.all(20),
+        child: Container(
+          height: 200,
+          alignment: Alignment.topCenter,
+          child: Container(
+            height: 200,
+            width: double.infinity,
+            child: Material(
+              elevation: 1,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              color: Colors.grey[900],
+            ),
+          ),
+        ),
+        controller: controller,
+      ),
+    );
+  }
+}
