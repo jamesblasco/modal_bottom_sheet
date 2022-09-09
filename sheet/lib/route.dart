1c41880fa945f1256da332f33c89ffdf1a30011d
Jaime Blasco <jaimeblasco97@gmail.com>
2022-09-09 11:19:18 +0200

    fix: format files

--- sheet/lib/route.dart
+++ sheet/lib/route.dart
@@ -1,4 +1,5 @@
 export 'src/route/base_route_extended.dart';
-export 'src/route/cupertino/sheet_route.dart' hide CupertinoSheetBottomRouteTransition;
+export 'src/route/cupertino/sheet_route.dart'
+    hide CupertinoSheetBottomRouteTransition;
 export 'src/route/delegated_transitions_route.dart';
 export 'src/route/sheet_route.dart';
