62f573096d88287d47bf832e3b3cd57f95dab507
netos23 <nikmoroz2002@outlook.com>
2023-01-25 12:17:50 +0300

    fix: hide flutter imports

--- modal_bottom_sheet/example/lib/modals/circular_modal.dart
+++ modal_bottom_sheet/example/lib/modals/circular_modal.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide ModalBottomSheetRoute;
 import 'package:flutter/services.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
